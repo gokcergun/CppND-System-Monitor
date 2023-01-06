02ec56e9a5e6098992acfec324195271e16bc7ce
Gokce Ergun <gokcergun@gmail.com>
2023-01-06 22:36:30 +0100

    delete question

--- src/format.cpp
+++ src/format.cpp
@@ -14,10 +14,6 @@ string Format::ElapsedTime(long seconds) {
     int hour = seconds/3600;
     int minute = seconds%3600/60;
     int sec = seconds%3600%60;
-    /*
-    Question: In order to be expressive about code, 
-    I prefer here creating variables instead of returning them directly.
-    But is it a good habit? Or do I waste memory?
-    */
+    
     return std::to_string(hour) + ":" + std::to_string(minute) + ":" + std::to_string(sec); 
     }
