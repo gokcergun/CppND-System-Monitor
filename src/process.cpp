b5f847df9e642b4a2b8f638a5528be46fdb4204f
Gokce Ergun <gokcergun@gmail.com>
2023-01-06 22:33:07 +0100

    check command size, replace ... for >30 chars + delete unnecessary regex_match()

--- src/process.cpp
+++ src/process.cpp
@@ -35,7 +35,9 @@ float Process::CpuUtilization() { return cpuUtilization_;}
 
 // Setter and Getter for the command that generated this process
 void Process::Command(int pid) { command_ = LinuxParser::Command(pid);}
-string Process::Command() { return command_; }
+string Process::Command() { 
+  if (command_.size() > 30){ command_ = command_.substr(0, 30) + "..."; }
+  return command_; }
 
 // Setter and Getter for this process's memory utilization
 void Process::Ram(int pid) { ram_ = LinuxParser::Ram(pid); }
@@ -52,20 +54,9 @@ long int Process::UpTime() { return upTime_; }
 // Overload the "less than" comparison operator for Process objects
 bool Process::operator<(Process const& other) const { 
     // compare self with the other (lhand of < )
-    float ramself  {0};
-    float ramother {0};
-    std::regex r("^[0-9]*(\\.[0-9]*)?$");
-    if (std::regex_match(ram_, r) && (std::regex_match(other.ram_, r))){
-        ramself = std::stof(ram_);
-        ramother = std::stof(other.ram_);
-    } else if (std::regex_match(ram_, r)) {
-        ramself = std::stof(ram_);
-    } else if (std::regex_match(other.ram_, r)) {
-        ramother = std::stof(ram_);
-    } 
 
-    if ( ramself== ramother){
+    if ( ram_ == other.ram_){
         return cpuUtilization_ < other.cpuUtilization_;
     }
-    return ramself < ramother;
+    return ram_ < other.ram_;
 }
